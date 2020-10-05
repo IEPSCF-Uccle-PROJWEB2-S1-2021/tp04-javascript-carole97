84ac0d5185312f641ce525f865e4e616a3717ddc
52060 <52060@etu.he2b.be git config --globla user.name carole>
2020-10-05 20:27:42 +0200

    min

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let a;
-let b;
-if(a<b){
-    console.log("le plus petit element est "+a);
+function minimum(a,b){ 
 
-}else if {
-    console.log("le plus petit element est "+b);
-
-} else{
-    console.log(a=b);
-}
+    if(a==b){
+        console.log(a +"a la même valeur que  "+b));
+    
+    }else if(a<b) {
+        console.log("le plus petit element est "+a);
+    
+    } else{
+        console.log("le plus petit element est "+b);
+    } }
